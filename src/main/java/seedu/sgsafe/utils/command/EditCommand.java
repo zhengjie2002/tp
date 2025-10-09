42bbddb3f6d790b88037736490e62e404c1777a2
shennontay <shennontay@gmail.com>
2025-10-09 17:35:05 +0800

    Add EditCommand class

---
+++ src/main/java/seedu/sgsafe/utils/command/EditCommand.java
@@ -0,0 +1,34 @@
+package seedu.sgsafe.utils.command;
+
+import java.util.Map;
+
+/**
+ * Represents a command to edit an existing case in the SGSafe system.
+ * This command stores the case number to identify which case to update
+ * and a set of new field values to replace the old ones.
+ */
+public class EditCommand extends Command {
+    // The case number of the case to edit
+    private final int caseNumber;
+
+    // Map of new field values, where the key is the flag (e.g. "title", "date")
+    private final Map<String, String> newFlagValues;
+
+    // Constructor that sets the case number and new field values
+    public EditCommand(int caseNumber, Map<String, String> newFlagValues) {
+        this.commandType = CommandType.EDIT;
+        this.caseNumber = caseNumber;
+        this.newFlagValues = newFlagValues;
+    }
+
+    // Returns the case number of the case to edit
+    public int getCaseNumber() {
+        return caseNumber;
+    }
+
+    // Returns the map of new field values
+    public Map<String, String> getNewFlagValues() {
+        return newFlagValues;
+    }
+}
+
