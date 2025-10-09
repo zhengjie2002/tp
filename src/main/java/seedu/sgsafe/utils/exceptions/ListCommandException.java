54bd9e0757f8b6b27d68f92d55f8e45ebecbf460
xelisce <xelisce@gmail.com>
2025-10-09 15:38:07 +0800

    Fix check error: Checkstyle

--- src/main/java/seedu/sgsafe/utils/exceptions/ListCommandException.java
+++ src/main/java/seedu/sgsafe/utils/exceptions/ListCommandException.java
@@ -1,9 +1,9 @@
 package seedu.sgsafe.utils.exceptions;
 
 public class ListCommandException extends InvalidCommandException {
-    static private final String ERROR_MESSAGE = "Your list command format is incorrect.";
-    static private final String CORRECT_FORMAT_MESSAGE = "The correct format for a list command is";
-    static private final String EXAMPLE_COMMAND = "list";
+    private static final String ERROR_MESSAGE = "Your list command format is incorrect.";
+    private static final String CORRECT_FORMAT_MESSAGE = "The correct format for a list command is";
+    private static final String EXAMPLE_COMMAND = "list";
 
     public ListCommandException() {
         super(ERROR_MESSAGE,
