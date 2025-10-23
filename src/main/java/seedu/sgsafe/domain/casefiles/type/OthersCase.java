dc63137d48692298cd60e56dad0d8748327ec57b
meiyi <e1385500@u.nus.edu>
2025-10-23 22:12:40 +0800

    Create Enum for case category and type

--- src/main/java/seedu/sgsafe/domain/casefiles/type/OthersCase.java
+++ src/main/java/seedu/sgsafe/domain/casefiles/type/OthersCase.java
@@ -1,12 +1,12 @@
 package seedu.sgsafe.domain.casefiles;
 
+import seedu.sgsafe.domain.casefiles.type.CaseCategory;
+
 public class OthersCase extends Case {
+    private String CustomCategory;
+
     public OthersCase(String id, String title, String date, String info, String victim, String officer) {
         super(id, title, date, info, victim, officer);
-    }
-
-    @Override
-    public String getCategory() {
-        return "Others";
+        this.category = CaseCategory.OTHERS;
     }
 }
--- src/main/java/seedu/sgsafe/domain/casefiles/type/financial/TheftCase.java
+++ src/main/java/seedu/sgsafe/domain/casefiles/type/financial/TheftCase.java
@@ -1,12 +1,12 @@
 package seedu.sgsafe.domain.casefiles;
 
+import seedu.sgsafe.domain.casefiles.type.CaseCategory;
+
 public class TheftCase extends Case {
+    private String stolenObject;
+
     public TheftCase(String id, String title, String date, String info, String victim, String officer) {
         super(id, title, date, info, victim, officer);
-    }
-
-    @Override
-    public String getCategory() {
-        return "Theft";
+        this.category = CaseCategory.THEFT;
     }
 }
