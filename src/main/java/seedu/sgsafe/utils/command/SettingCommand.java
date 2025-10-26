26df9b7ec2d03d413a7c87125d7c4e3e24f46df2
zhengjie2002 <tan.zhengjie6@gmail.com>
2025-10-26 21:49:53 +0800

    Fix bug allowing invalid date format

--- src/main/java/seedu/sgsafe/utils/command/SettingCommand.java
+++ src/main/java/seedu/sgsafe/utils/command/SettingCommand.java
@@ -25,7 +25,7 @@ public class SettingCommand extends Command {
             try {
                 Settings.setInputDateFormat(newDateFormat);
                 Display.printMessage("Date input format updated to: " + newDateFormat);
-            } catch (IllegalArgumentException e) {
+            } catch (Exception e) {
                 // Rollback to previous date format
                 Settings.setInputDateFormat(previousDateFormat);
                 Display.printMessage("Invalid date format. Reverted to previous format: " + previousDateFormat);
@@ -36,7 +36,7 @@ public class SettingCommand extends Command {
             try {
                 Settings.setOutputDateFormat(newDateFormat);
                 Display.printMessage("Date output format updated to: " + newDateFormat);
-            } catch (IllegalArgumentException e) {
+            } catch (Exception e) {
                 // Rollback to previous date format
                 Settings.setOutputDateFormat(previousDateFormat);
                 Display.printMessage("Invalid date format. Reverted to previous format: " + previousDateFormat);
--- src/main/java/seedu/sgsafe/utils/settings/Settings.java
+++ src/main/java/seedu/sgsafe/utils/settings/Settings.java
@@ -1,8 +1,15 @@
 package seedu.sgsafe.utils.settings;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * The `Settings` class provides utility methods to manage input and output date formats.
+ * It allows setting and retrieving date formats while ensuring the formats are valid.
+ */
 public class Settings {
+
+    // Default date format used for input and output
     private static final String DEFAULT_DATE_FORMAT = "dd/MM/yyyy";
     private static String inputDateFormat = DEFAULT_DATE_FORMAT;
     private static String outputDateFormat = DEFAULT_DATE_FORMAT;
@@ -15,15 +22,31 @@ public class Settings {
         return outputDateFormat;
     }
 
-    public static void setOutputDateFormat(String format) throws IllegalArgumentException {
-        // Validate format by attempting to create a DateTimeFormatter
-        DateTimeFormatter.ofPattern(format);
+    /**
+     * Sets the output date format.
+     * Validates the provided format by attempting to create a `DateTimeFormatter`.
+     *
+     * @param format the new output date format as a `String`.
+     */
+    public static void setOutputDateFormat(String format) {
+        // Validate format by attempting to create a DateTimeFormatter and parse. If it fails it will throw an exception
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        LocalDate testDate = LocalDate.of(2024, 1, 15);
+        testDate.format(formatter);
         outputDateFormat = format;
     }
 
-    public static void setInputDateFormat(String format) throws IllegalArgumentException {
-        // Validate format by attempting to create a DateTimeFormatter
-        DateTimeFormatter.ofPattern(format);
+    /**
+     * Sets the input date format.
+     * Validates the provided format by attempting to create a `DateTimeFormatter`.
+     *
+     * @param format the new input date format as a `String`.
+     */
+    public static void setInputDateFormat(String format) {
+        // Validate format by attempting to create a DateTimeFormatter and parse. If it fails it will throw an exception
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        LocalDate testDate = LocalDate.of(2024, 1, 15);
+        testDate.format(formatter);
         inputDateFormat = format;
     }
 }
