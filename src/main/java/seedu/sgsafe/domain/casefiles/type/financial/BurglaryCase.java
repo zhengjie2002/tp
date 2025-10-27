05a15d82f95bc9262616e78c38a4ae7951b996b6
meiyi <e1385500@u.nus.edu>
2025-10-27 17:51:26 +0800

    Update burglary class

--- src/main/java/seedu/sgsafe/domain/casefiles/type/financial/BurglaryCase.java
+++ src/main/java/seedu/sgsafe/domain/casefiles/type/financial/BurglaryCase.java
@@ -2,8 +2,10 @@ package seedu.sgsafe.domain.casefiles.type.financial;
 
 import seedu.sgsafe.domain.casefiles.type.CaseCategory;
 
+import java.time.LocalDate;
+
 public class BurglaryCase extends FinancialCase {
-    public BurglaryCase(String id, String title, String date, String info, String victim, String officer) {
+    public BurglaryCase(String id, String title, LocalDate date, String info, String victim, String officer) {
         super(id, title, date, info, victim, officer);
         this.category = CaseCategory.BURGLARY;
         this.categoryString = "Burglary";
