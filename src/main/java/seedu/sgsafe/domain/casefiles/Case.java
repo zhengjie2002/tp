f85d295d65620578c93e9dceefb130850f5f77f7
shennontay <shennontay@gmail.com>
2025-10-27 22:06:05 +0800

    Fix checkstyle

--- src/main/java/seedu/sgsafe/domain/casefiles/Case.java
+++ src/main/java/seedu/sgsafe/domain/casefiles/Case.java
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
