44aad60acbfd6e152fad71d803d912f245f8a11f
zhengjie2002 <tan.zhengjie6@gmail.com>
2025-10-27 14:40:32 +0800

    Update text-ui-test

--- src/main/java/seedu/sgsafe/utils/exceptions/InvalidDateInputException.java
+++ src/main/java/seedu/sgsafe/utils/exceptions/InvalidDateInputException.java
@@ -5,7 +5,7 @@ import seedu.sgsafe.utils.settings.Settings;
 public class InvalidDateInputException extends InvalidCommandException {
     private static final String ERROR_MESSAGE = "The date input format is invalid.";
     private static final String INSTRUCTION =
-            "Please enter the date in the format: " + Settings.getInputDateFormat() + ". ";
+            "Please enter the date in the format: " + Settings.getInputDateFormat() + ".";
     private static final String TIP2 = "You may change this in the settings.";
 
     public InvalidDateInputException() {
