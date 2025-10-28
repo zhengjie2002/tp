263cf443f4c3a44c31427b9548fa8d0adec1973a
shennontay <shennontay@gmail.com>
2025-10-28 20:48:03 +0800

    Correct exception tip

--- src/main/java/seedu/sgsafe/utils/exceptions/InvalidHelpCommandException.java
+++ src/main/java/seedu/sgsafe/utils/exceptions/InvalidHelpCommandException.java
@@ -4,7 +4,7 @@ public class InvalidHelpCommandException extends InvalidCommandException {
 
     private static final String ERROR_MESSAGE = "The 'help' command does not take any arguments.";
     private static final String USAGE = "Usage: help";
-    private static final String TIP = "Simply type 'help' to exit the application.";
+    private static final String TIP = "Simply type 'help' to display the help manual.";
 
     public InvalidHelpCommandException() {
         super(ERROR_MESSAGE, USAGE, TIP);
