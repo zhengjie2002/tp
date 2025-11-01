8a01c1ec00f50b708a7589ae6024db2ba59dcc59
meiyi <e1385500@u.nus.edu>
2025-11-02 01:48:29 +0800

    Change message format for InvalidEditCommandException

--- src/main/java/seedu/sgsafe/utils/exceptions/InvalidEditCommandException.java
+++ src/main/java/seedu/sgsafe/utils/exceptions/InvalidEditCommandException.java
@@ -1,12 +1,12 @@
 package seedu.sgsafe.utils.exceptions;
 
 public class InvalidEditCommandException extends InvalidCommandException {
-    private static final String ERROR_MESSAGE = "Your edit command format is incorrect." +
-            "The 'edit' command requires a valid case ID. " +
+    private static final String ERROR_MESSAGE = "Your edit command format is incorrect. ";
+    private static final String TIP = "The 'edit' command requires a valid case ID. " +
             "Flags are optional but must be paired with their new values.";
-    private static final String TIP = "For example, try: \"edit 000000\" or \"edit 000000 --title new title \"";
+    private static final String EXAMPLE = "For example, try: \"edit 000000\" or \"edit 000000 --title new title \"";
 
     public InvalidEditCommandException() {
-        super(ERROR_MESSAGE, TIP);
+        super(ERROR_MESSAGE, TIP, EXAMPLE);
     }
 }
